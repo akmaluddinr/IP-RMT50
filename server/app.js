031cb7edb7483757f98d95c0c58021c8d544c1d6
akmaluddinr <ridwanakmaluddin@gmail.com>
2024-07-11 10:09:00 +0700

    feat: Add CORS

--- server/app.js
+++ server/app.js
@@ -3,7 +3,9 @@ const express = require("express");
 const errorHandler = require("./middlewares/errorHandler");
 const app = express();
 const port = 3000;
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
